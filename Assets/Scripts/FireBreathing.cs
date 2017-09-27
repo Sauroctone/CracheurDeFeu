486b90c60d7dc7ee1d95d63d33e2d6b082526fc9
Sauroctone <neo.laq.33@gmail.com>
2017-09-27 18:12:20 +0200

    Fire orientation
    Fire orientation : done
    Player orientation relative to fire orientation : WIP

--- Assets/Scripts/FireBreathing.cs
+++ Assets/Scripts/FireBreathing.cs
@@ -7,17 +7,22 @@ public class FireBreathing : MonoBehaviour {
 	public ParticleSystem fireBreath;
 	private PlayerController player;
 	private BottleManager bottles;
+	private float rightStickX;
+	private float rightStickY;
+	private float angle;
+	private float scaleX;
 
 	void Start()
 	{
 		player = GetComponent<PlayerController> ();
 		bottles = GetComponent<BottleManager> ();
+		scaleX = transform.localScale.x;
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
-		if (!fireBreath.isPlaying && Input.GetKeyDown ("e") && bottles.bottleCount > 0) 
+		if (!fireBreath.isPlaying && Input.GetButtonDown ("BreatheFire") && bottles.bottleCount > 0) 
 		{
 			fireBreath.Play ();
 			player.speed = player.speedBreathing;
@@ -26,5 +31,21 @@ public class FireBreathing : MonoBehaviour {
 
 		if (!fireBreath.isEmitting)
 			player.speed = player.speedWalking;
+
+		rightStickX = Input.GetAxis("RS_Vertical");
+		rightStickY = Input.GetAxis("RS_Horizontal");
+		angle = Mathf.Atan2(rightStickX, rightStickY) * Mathf.Rad2Deg;
+
+		if (rightStickX != 0 || rightStickY != 0) 
+		{
+			fireBreath.transform.eulerAngles = new Vector3 (-180 + angle, -90, 90);
+		}
+
+		//REGLER CA
+		if (fireBreath.transform.eulerAngles.x > (-180 + 90) || fireBreath.transform.eulerAngles.x < (-180 -90)) 
+		{
+			print ("hop");
+			transform.localScale = new Vector3 (-scaleX, transform.localScale.y, transform.localScale.z);
+		}
 	}
 }
