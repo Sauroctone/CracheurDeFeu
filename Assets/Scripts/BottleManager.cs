3d8f222c08a4b86505101b86ae893a95dade4f6b
Sauroctone <neo.laq.33@gmail.com>
2017-10-02 17:17:01 +0200

    Firebreathing update + bouteilles
    Nouveau système de feu
    - contrôle quantité de feu
    - plus on appuie plus ça va loin
    
    --> augmenter le nombre de particules ?

--- Assets/Scripts/BottleManager.cs
+++ Assets/Scripts/BottleManager.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class BottleManager : MonoBehaviour {
 
-	public int bottleCount;
+	public float alcoholCount;
+
+	void Update()
+	{
+		if (alcoholCount < 0)
+			alcoholCount = 0;
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag == "Alcohol") 
+		{
+			alcoholCount += 3;
+
+			GameObject.Destroy (col.gameObject);
+		}
+	}
 }
--- Assets/Scripts/FireBreathing.cs
+++ Assets/Scripts/FireBreathing.cs
@@ -7,29 +7,55 @@ public class FireBreathing : MonoBehaviour {
 	public ParticleSystem fireBreath;
 	private PlayerController player;
 	private BottleManager bottles;
+
 	private float rightStickX;
 	private float rightStickY;
 	private float angle;
 	private float scaleX;
 	private float fireRotationX;
+	public float initStartRotLow;
+	public float initStartRotHigh;
+	public float startRotLow;
+	public float startRotHigh;
+
 	public GameObject playerVisuals;
 	public GameObject fireRotator;
+
 
 	void Start()
 	{
 		player = GetComponent<PlayerController> ();
 		bottles = GetComponent<BottleManager> ();
 		scaleX = playerVisuals.transform.localScale.x;
+		startRotLow = initStartRotLow;
+		startRotHigh = initStartRotHigh;
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
-		if (!fireBreath.isPlaying && Input.GetButtonDown ("BreatheFire") && bottles.bottleCount > 0) 
+		if (!fireBreath.isPlaying && Input.GetButtonDown ("BreatheFire") && bottles.alcoholCount > 0) 
 		{
 			fireBreath.Play ();
 			player.speed = player.speedBreathing;
-			bottles.bottleCount -= 1;
+		}
+
+		if (Input.GetButton ("BreatheFire") && bottles.alcoholCount > 0) 
+		{
+			bottles.alcoholCount -= Time.deltaTime;
+			var main = fireBreath.main;
+			//main.startRotation = fireRotator.transform.eulerAngles.y;
+			startRotLow += 0.1f * Time.deltaTime;
+			startRotHigh += 0.1f * Time.deltaTime;
+			main.startLifetime = new ParticleSystem.MinMaxCurve (startRotLow, startRotHigh);
+		}
+
+		if (fireBreath.isPlaying && Input.GetButtonUp ("BreatheFire") || fireBreath.isPlaying && bottles.alcoholCount <= 0) 
+		{
+			fireBreath.Stop ();
+			var main = fireBreath.main;
+			startRotLow = initStartRotLow;
+			startRotHigh = initStartRotHigh;
 		}
 
 		if (!fireBreath.isEmitting)
