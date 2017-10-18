9213016b4a86b595352762682ae0775947950571
Sauroctone <neo.laq.33@gmail.com>
2017-10-18 16:44:41 +0200

    Camera follow + torchlight prefab
    - Camera follows the player ; NEED to clamp it to edge of background
    
    - torchlight prefab for strategic lights in LD

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+	public Transform target;
+	public float cameraLerp;
+	private Vector3 velocity = Vector3.zero;
+
+	void Update () 
+	{
+		transform.position = Vector3.SmoothDamp (transform.position, new Vector3 (target.position.x, target.position.y, transform.position.z), ref velocity, cameraLerp);
+
+	}
+}
